80459a3e8902abc7bfdb9d90f2968f79dad9097d
felipeFerreiraffl <felipado2x0@gmail.com>
2025-07-29 10:12:18 -0300

    feat: new project for training

---
+++ projects/users/src/controller/usersController.js
@@ -0,0 +1,5 @@
+const users = [
+  { id: 1, name: "Fernando Fernandes", email: "fernandfer05@email.com" },
+  { id: 2, name: "Marilene Augusto", email: "marilene_augusto@email.com" },
+  { id: 3, name: "Roberto Humberto", email: "robertin2222@email.com" },
+];
